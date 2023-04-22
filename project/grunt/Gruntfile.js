8c7dd337a9a1098f0010c54188905022ce2cfc0d
praveen <mspraveenkumar77@gmail.com>
2023-04-22 04:38:22 +0000

    InitialCommit

---
+++ project/grunt/Gruntfile.js
@@ -0,0 +1,9 @@
+module.exports = function(grunt){
+    grunt.registerTask('default', function(){
+        console.log('I am Default Grunt, Running !!');
+    });
+
+    grunt.registerTask('helloWorld', function(){
+        console.log('I am Grunt, Running !!');
+    });
+};
